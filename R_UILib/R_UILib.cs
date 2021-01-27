c8cbd177f365438ec95e4ecae0f919d3c6f7e860
BuildTools <unconfigured@null.spigotmc.org>
2021-01-27 23:33:19 +0900

    R_UILibの変更(途中)

--- R_UILib/R_UILib.cs
+++ R_UILib/R_UILib.cs
@@ -117,8 +117,20 @@ namespace R_UILib
                 }
             }
         }
+        
+        protected static bool MouseClickUpLeftDetection(int x1, int y1, int x2, int y2)
+        {
+            if (MouseClickUpLeft == true && MouseClickUpLeftPointX >= x1 && MouseClickUpLeftPointX <= x2 && MouseClickUpLeftPointY >= x1 && MouseClickUpLeftPointY <= y2 && MouseClickDownLeftPointX >= x1 && MouseClickDownLeftPointX <= x2 && MouseClickDownLeftPointY >= y1 && MouseClickDownLeftPointY <= y2) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        
     }
 
+    // RUI_Buttonクラス
     abstract class RUI_Button : RUI_MouseData
     {
         protected static int X1 = -1;
@@ -131,16 +143,12 @@ namespace R_UILib
         protected static string Str = "";
 
         //左クリックアップの判定
-        protected static bool MouseClickUpLeftDetection()
+        protected static bool LeftUpDetection()
         {
-            if (MouseClickUpLeft == true && MouseClickUpLeftPointX >= X1 && MouseClickUpLeftPointX <= X2 && MouseClickUpLeftPointY >= Y1 && MouseClickUpLeftPointY <= Y2 && MouseClickDownLeftPointX >= X1 && MouseClickDownLeftPointX <= X2 && MouseClickDownLeftPointY >= Y1 && MouseClickDownLeftPointY <= Y2) {
-                return true;
-            }
-            else {
-                return false;
-            }
+            return MouseClickUpLeftDetection(X1, Y1, X2, Y2);
         }
     }
+    // RUIクラス
     abstract class RUI : RUI_MouseData
     {
         // マウス情報の更新
@@ -149,169 +157,6 @@ namespace R_UILib
         {
             RUI_MouseData.UptadeMouseState();
         }
-
-        /*ppp
-        private static bool ClickUpLeftDetection(int x1, int y1, int x2, int y2)
-        {
-            if (MouseClickUpLeftPointX >= x1 && MouseClickUpLeftPointX <= x2 && MouseClickUpLeftPointY >= y1 && MouseClickUpLeftPointY <= y2 && MouseClickDownLeftPointX >= x1 && MouseClickDownLeftPointX <= x2 && MouseClickDownLeftPointY >= y1 && MouseClickDownLeftPointY <= y2)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        */
-        /*
-        public static void UptadeMouseState()
-        {
-            OldMouseClickLeft = MouseClickLeft;
-            OldMouseClickRight = MouseClickRight;
-            //クリックしているかどうか判定
-            if ((DX.GetMouseInput() & DX.MOUSE_INPUT_LEFT) != 0)
-            {
-                MouseClickLeft = true;
-            }
-            else
-            {
-                MouseClickLeft = false;
-            }
-            if ((DX.GetMouseInput() & DX.MOUSE_INPUT_RIGHT) != 0)
-            {
-                MouseClickRight = true;
-            }
-            else
-            {
-                MouseClickRight = false;
-            }
-            //クリックした瞬間を判定
-            if (OldMouseClickLeft == false && MouseClickLeft == true)
-            {
-                MouseClickDownLeft = true;
-            }
-            else
-            {
-                MouseClickDownLeft = false;
-            }
-            if (OldMouseClickRight == false && MouseClickRight == true)
-            {
-                MouseClickDownRight = true;
-            }
-            else
-            {
-                MouseClickDownRight = false;
-            }
-            //クリックを離した瞬間を判定
-            if (OldMouseClickLeft == true && MouseClickLeft == false)
-            {
-                MouseClickUpLeft = true;
-            }
-            else
-            {
-                MouseClickUpLeft = false;
-            }
-            if (OldMouseClickRight == true && MouseClickRight == false)
-            {
-                MouseClickUpRight = true;
-            }
-            else
-            {
-                MouseClickUpRight = false;
-            }
-            //マウス座標取得
-            DX.GetMousePoint(out MousePointX, out MousePointY);
-            //左クリックした瞬間のマウス座標取得
-            if (MouseClickDownLeft == true) {
-                DX.GetMousePoint(out MouseClickDownLeftPointX, out MouseClickDownLeftPointY);
-            }
-            //左クリックを離した瞬間のマウス座標取得
-            if (MouseClickUpLeft == true) {
-                DX.GetMousePoint(out MouseClickUpLeftPointX, out MouseClickUpLeftPointY);
-            }
-            //右クリックした瞬間のマウス座標取得
-            if (MouseClickDownRight == true)
-            {
-                DX.GetMousePoint(out MouseClickDownRightPointX, out MouseClickDownRightPointY);
-            }
-            //右クリックを離した瞬間のマウス座標取得
-            if (MouseClickUpRight == true)
-            {
-                DX.GetMousePoint(out MouseClickUpRightPointX, out MouseClickUpRightPointY);
-            }
-        }
-    
-        public static bool UI_Image(int x1, int y1, int x2, int y2, int grhandle)
-        {
-            DX.DrawExtendGraph(x1, y2, x2, y2, grhandle, DX.TRUE);
-            if (MouseClickUpLeftDetection(x1, y1, x2, y2) == true)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        public static bool UI_StringImage(int x1, int y1, int x2, int y2, string str, int fonthandle, uint color)
-        {
-            DX.DrawString(x1+(((x2 - x1) - DX.GetDrawStringWidthToHandle(str, str.Length, fonthandle)) / 2), y1+(((y2-y1) - DX.GetFontSizeToHandle(fonthandle)) / 2), str, color);
-            if (MouseClickUpLeftDetection(x1, y1, x2, y2) == true)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        public static bool UI_StringImage(int x1, int y1, int x2, int y2, string str, int fonthandle, uint color, int grhandle)
-        {
-            DX.DrawExtendGraph(x1, y1, x2, y2, grhandle, DX.TRUE);
-            DX.DrawString(x1 + (((x2 - x1) - DX.GetDrawStringWidthToHandle(str, str.Length, fonthandle)) / 2), y1 + (((y2 - y1) - DX.GetFontSizeToHandle(fonthandle)) / 2), str, color);
-            if (MouseClickUpLeftDetection(x1, y1, x2, y2) == true)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        public static bool UI_String(int x1, int y1, string str, int fonthandle, uint color)
-        {
-            int x2 = x1 + DX.GetDrawStringWidthToHandle(str, str.Length, fonthandle);
-            int y2 = y1 + DX.GetFontSizeToHandle(fonthandle);
-            DX.DrawString(x1, y1, str, color);
-
-            if (MouseClickUpLeftDetection(x1, y1, x2, y2) == true)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        public static bool UI_String(int x1, int y1, string str, int fonthandle, uint color, int grhandle)
-        {
-            int x2 = x1 + DX.GetDrawStringWidthToHandle(str, str.Length, fonthandle);
-            int y2 = y1 + DX.GetFontSizeToHandle(fonthandle);
-            DX.DrawExtendGraph(x1, y1, x2, y2, grhandle, DX.TRUE);
-            DX.DrawString(x1, y1, str, color);
-            if (MouseClickUpLeftDetection(x1, y1, x2, y2) == true)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        // x1 y1 x2 y2 grhandle 
-        // 1クリックで数回判定される
-        // 長押し用も必要
-        */
     }
 
 
@@ -392,3 +237,4 @@ namespace R_UILib
         }
     }
 }
+
